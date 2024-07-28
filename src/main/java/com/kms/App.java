6e79a8ba055cf30023898b0c6072f6b12af43d23
Do Minh Quang <quangdo@kms-technology.com>
2024-07-28 19:38:26 +0700

    feat: add log service Logback

--- src/main/java/com/kms/App.java
+++ src/main/java/com/kms/App.java
@@ -1,13 +1,16 @@
 package com.kms;
 
-/**
- * Hello world!
- *
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App 
 {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.info("This is an info message.");
+        logger.error( "This is an error message");
+        logger.debug("This is debug message");
     }
 }
